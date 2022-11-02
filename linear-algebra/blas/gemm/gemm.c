4e2649820c4b3417bb3ab428416c9336297d2e37
Georg Streich <streichg@ethz.ch>
2022-11-02 14:29:40 +0100

    Add optimized kernel

--- linear-algebra/blas/gemm/gemm.c
+++ linear-algebra/blas/gemm/gemm.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
+#include <immintrin.h>
 
 /* Include polybench common header. */
 #include <polybench.h>
@@ -65,11 +66,138 @@ void print_array(int ni, int nj,
   POLYBENCH_DUMP_FINISH;
 }
 
+#define BI 20
+#define BJ 40
+#define BK 20
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
 static
 void kernel_gemm(int ni, int nj, int nk,
+		 DATA_TYPE alpha,
+		 DATA_TYPE beta,
+		 DATA_TYPE POLYBENCH_2D(C,NI,NJ,ni,nj),
+		 DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		 DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj))
+{
+#pragma scop
+
+  __m256d valpha = _mm256_set1_pd(alpha);
+
+  #pragma omp parallel for
+  for (int i = 0; i < _PB_NI - BI + 1; i += BI) {
+    int j;
+    for (j = 0; j < _PB_NJ - BJ + 1; j += BJ) {
+      for (int u = i; u < i + BI; u++) {
+        for (int v = j; v < j + BI; v++) {
+          C[u][v] *= beta;
+        }
+      }
+
+      int k;
+      for (k = 0; k < _PB_NK - BK + 1; k += BK) {
+        for (int u = i; u < i + BI; u += 4) {
+          for (int v = j; v < j + BJ; v += 8) {
+            __m256d ab00 = _mm256_set1_pd(0.0);
+            __m256d ab01 = _mm256_set1_pd(0.0);
+            __m256d ab02 = _mm256_set1_pd(0.0);
+            __m256d ab03 = _mm256_set1_pd(0.0);
+
+            __m256d ab10 = _mm256_set1_pd(0.0);
+            __m256d ab11 = _mm256_set1_pd(0.0);
+            __m256d ab12 = _mm256_set1_pd(0.0);
+            __m256d ab13 = _mm256_set1_pd(0.0);
+
+            for (int w = k; w < k + BK; w++) {
+              __m256d a0 = _mm256_set1_pd(A[u + 0][w]);
+              __m256d a1 = _mm256_set1_pd(A[u + 1][w]);
+              __m256d a2 = _mm256_set1_pd(A[u + 2][w]);
+              __m256d a3 = _mm256_set1_pd(A[u + 3][w]);
+
+              __m256d b0 = _mm256_load_pd(&B[w][v + 0]);
+              __m256d b1 = _mm256_load_pd(&B[w][v + 4]);
+
+              ab00 = _mm256_fmadd_pd(a0, b0, ab00);
+              ab01 = _mm256_fmadd_pd(a1, b0, ab01);
+              ab02 = _mm256_fmadd_pd(a2, b0, ab02);
+              ab03 = _mm256_fmadd_pd(a3, b0, ab03);
+
+              ab10 = _mm256_fmadd_pd(a0, b1, ab10);
+              ab11 = _mm256_fmadd_pd(a1, b1, ab11);
+              ab12 = _mm256_fmadd_pd(a2, b1, ab12);
+              ab13 = _mm256_fmadd_pd(a3, b1, ab13);
+            }
+
+            __m256d c00 = _mm256_load_pd(&C[u + 0][v + 0]);
+            __m256d c01 = _mm256_load_pd(&C[u + 1][v + 0]);
+            __m256d c02 = _mm256_load_pd(&C[u + 2][v + 0]);
+            __m256d c03 = _mm256_load_pd(&C[u + 3][v + 0]);
+
+            __m256d c10 = _mm256_load_pd(&C[u + 0][v + 4]);
+            __m256d c11 = _mm256_load_pd(&C[u + 1][v + 4]);
+            __m256d c12 = _mm256_load_pd(&C[u + 2][v + 4]);
+            __m256d c13 = _mm256_load_pd(&C[u + 3][v + 4]);
+
+            c00 = _mm256_fmadd_pd(valpha, ab00, c00);
+            c01 = _mm256_fmadd_pd(valpha, ab01, c01);
+            c02 = _mm256_fmadd_pd(valpha, ab02, c02);
+            c03 = _mm256_fmadd_pd(valpha, ab03, c03);
+
+            c10 = _mm256_fmadd_pd(valpha, ab10, c10);
+            c11 = _mm256_fmadd_pd(valpha, ab11, c11);
+            c12 = _mm256_fmadd_pd(valpha, ab12, c12);
+            c13 = _mm256_fmadd_pd(valpha, ab13, c13);
+
+            _mm256_store_pd(&C[u + 0][v + 0], c00);
+            _mm256_store_pd(&C[u + 1][v + 0], c01);
+            _mm256_store_pd(&C[u + 2][v + 0], c02);
+            _mm256_store_pd(&C[u + 3][v + 0], c03);
+
+            _mm256_store_pd(&C[u + 0][v + 4], c10);
+            _mm256_store_pd(&C[u + 1][v + 4], c11);
+            _mm256_store_pd(&C[u + 2][v + 4], c12);
+            _mm256_store_pd(&C[u + 3][v + 4], c13);
+          }
+        }
+      }
+
+      for (; k < _PB_NK; k++) {
+        for (int u = i; u < i + BI; u++) {
+          for (int v = j; v < j + BJ; v++) {
+            C[u][v] += alpha * A[u][k] * B[k][v];
+          }
+        }
+      }
+    }
+
+    for (; j < _PB_NJ; j++) {
+      for (int u = i; u < i + BI; u++) {
+        C[u][j] *= beta;
+      }
+
+      for (int k = 0; k < _PB_NK; k++) {
+        for (int u = i; u < i + BI; u++) {
+          C[u][j] += alpha * A[u][k] * B[k][j];
+        }
+      }
+    }
+  }
+
+  int i = BI * (_PB_NI / BI);
+  for (; i < _PB_NI; i++) {
+    for (int j = 0; j < _PB_NJ; j++) {
+      C[i][j] *= beta;
+      for (int k = 0; k < _PB_NK; k++) {
+        C[i][j] += alpha * A[i][k] * B[k][j];
+      }
+    }
+  }
+#pragma endscop
+
+}
+
+static
+void kernel_gemm_original(int ni, int nj, int nk,
 		 DATA_TYPE alpha,
 		 DATA_TYPE beta,
 		 DATA_TYPE POLYBENCH_2D(C,NI,NJ,ni,nj),
@@ -98,13 +226,14 @@ void kernel_gemm(int ni, int nj, int nk,
 
 }
 
-
 int main(int argc, char** argv)
 {
   /* Retrieve problem size. */
   int ni = NI;
   int nj = NJ;
   int nk = NK;
+
+  polybench_program_total_flops = (double) ni * (double) nj * (2 * (double) nk - 1);
 
   /* Variable declaration/allocation. */
   DATA_TYPE alpha;
