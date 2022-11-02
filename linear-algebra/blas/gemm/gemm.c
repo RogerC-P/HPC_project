062c3f867dc01120eae5672bc98dd7ae52cf4f96
Georg Streich <streichg@ethz.ch>
2022-11-02 14:57:05 +0100

    Tune block size

--- linear-algebra/blas/gemm/gemm.c
+++ linear-algebra/blas/gemm/gemm.c
@@ -68,7 +68,7 @@ void print_array(int ni, int nj,
 
 #define BI 20
 #define BJ 40
-#define BK 20
+#define BK 12
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
@@ -81,7 +81,6 @@ void kernel_gemm(int ni, int nj, int nk,
 		 DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj))
 {
 #pragma scop
-
   __m256d valpha = _mm256_set1_pd(alpha);
 
   #pragma omp parallel for
@@ -161,11 +160,67 @@ void kernel_gemm(int ni, int nj, int nk,
         }
       }
 
-      for (; k < _PB_NK; k++) {
-        for (int u = i; u < i + BI; u++) {
-          for (int v = j; v < j + BJ; v++) {
-            C[u][v] += alpha * A[u][k] * B[k][v];
+      for (int u = i; u < i + BI; u += 4) {
+        for (int v = j; v < j + BJ; v += 8) {
+          __m256d ab00 = _mm256_set1_pd(0.0);
+          __m256d ab01 = _mm256_set1_pd(0.0);
+          __m256d ab02 = _mm256_set1_pd(0.0);
+          __m256d ab03 = _mm256_set1_pd(0.0);
+
+          __m256d ab10 = _mm256_set1_pd(0.0);
+          __m256d ab11 = _mm256_set1_pd(0.0);
+          __m256d ab12 = _mm256_set1_pd(0.0);
+          __m256d ab13 = _mm256_set1_pd(0.0);
+
+          for (int w = k; w < _PB_NK; w++) {
+            __m256d a0 = _mm256_set1_pd(A[u + 0][w]);
+            __m256d a1 = _mm256_set1_pd(A[u + 1][w]);
+            __m256d a2 = _mm256_set1_pd(A[u + 2][w]);
+            __m256d a3 = _mm256_set1_pd(A[u + 3][w]);
+
+            __m256d b0 = _mm256_load_pd(&B[w][v + 0]);
+            __m256d b1 = _mm256_load_pd(&B[w][v + 4]);
+
+            ab00 = _mm256_fmadd_pd(a0, b0, ab00);
+            ab01 = _mm256_fmadd_pd(a1, b0, ab01);
+            ab02 = _mm256_fmadd_pd(a2, b0, ab02);
+            ab03 = _mm256_fmadd_pd(a3, b0, ab03);
+
+            ab10 = _mm256_fmadd_pd(a0, b1, ab10);
+            ab11 = _mm256_fmadd_pd(a1, b1, ab11);
+            ab12 = _mm256_fmadd_pd(a2, b1, ab12);
+            ab13 = _mm256_fmadd_pd(a3, b1, ab13);
           }
+
+          __m256d c00 = _mm256_load_pd(&C[u + 0][v + 0]);
+          __m256d c01 = _mm256_load_pd(&C[u + 1][v + 0]);
+          __m256d c02 = _mm256_load_pd(&C[u + 2][v + 0]);
+          __m256d c03 = _mm256_load_pd(&C[u + 3][v + 0]);
+
+          __m256d c10 = _mm256_load_pd(&C[u + 0][v + 4]);
+          __m256d c11 = _mm256_load_pd(&C[u + 1][v + 4]);
+          __m256d c12 = _mm256_load_pd(&C[u + 2][v + 4]);
+          __m256d c13 = _mm256_load_pd(&C[u + 3][v + 4]);
+
+          c00 = _mm256_fmadd_pd(valpha, ab00, c00);
+          c01 = _mm256_fmadd_pd(valpha, ab01, c01);
+          c02 = _mm256_fmadd_pd(valpha, ab02, c02);
+          c03 = _mm256_fmadd_pd(valpha, ab03, c03);
+
+          c10 = _mm256_fmadd_pd(valpha, ab10, c10);
+          c11 = _mm256_fmadd_pd(valpha, ab11, c11);
+          c12 = _mm256_fmadd_pd(valpha, ab12, c12);
+          c13 = _mm256_fmadd_pd(valpha, ab13, c13);
+
+          _mm256_store_pd(&C[u + 0][v + 0], c00);
+          _mm256_store_pd(&C[u + 1][v + 0], c01);
+          _mm256_store_pd(&C[u + 2][v + 0], c02);
+          _mm256_store_pd(&C[u + 3][v + 0], c03);
+
+          _mm256_store_pd(&C[u + 0][v + 4], c10);
+          _mm256_store_pd(&C[u + 1][v + 4], c11);
+          _mm256_store_pd(&C[u + 2][v + 4], c12);
+          _mm256_store_pd(&C[u + 3][v + 4], c13);
         }
       }
     }
