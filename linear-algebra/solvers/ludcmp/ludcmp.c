27f3f574b5057f90e8c7de30f3ebcb7a02bd092c
Georg Streich <streichg@ethz.ch>
2022-12-19 09:15:27 +0100

    Spawn threads at start of programm

--- linear-algebra/solvers/ludcmp/ludcmp.c
+++ linear-algebra/solvers/ludcmp/ludcmp.c
@@ -156,25 +156,31 @@ int main(int argc, char** argv)
   printf("ludcmp\n####\n");
 
   int n0 = (N < (1 << 10)) ? N : (1 << 10);
-  for (int n = n0; n <= N; n <<= 1) {
-    printf("size %d:\n", n);
 
-    /* Variable declaration/allocation. */
-    POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
-    POLYBENCH_1D_ARRAY_DECL(b, DATA_TYPE, N, n);
-    POLYBENCH_1D_ARRAY_DECL(x, DATA_TYPE, N, n);
-    POLYBENCH_1D_ARRAY_DECL(y, DATA_TYPE, N, n);
+  /* Variable declaration/allocation. */
+  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
+  POLYBENCH_1D_ARRAY_DECL(b, DATA_TYPE, N, n);
+  POLYBENCH_1D_ARRAY_DECL(x, DATA_TYPE, N, n);
+  POLYBENCH_1D_ARRAY_DECL(y, DATA_TYPE, N, n);
+
+  #pragma omp parallel
+  for (int n = n0; n <= N; n <<= 1) {
+    #pragma omp master
+    printf("size %d:\n", n);
 
     for (int i = 0; i < N_RUNS; i++) {
       /* Initialize array(s). */
-      init_array (n,
-        POLYBENCH_ARRAY(A),
-        POLYBENCH_ARRAY(b),
-        POLYBENCH_ARRAY(x),
-        POLYBENCH_ARRAY(y));
+      #pragma omp master
+      {
+        init_array (n,
+          POLYBENCH_ARRAY(A),
+          POLYBENCH_ARRAY(b),
+          POLYBENCH_ARRAY(x),
+          POLYBENCH_ARRAY(y));
 
-      /* Start timer. */
-      polybench_start_instruments;
+        /* Start timer. */
+        polybench_start_instruments;
+      }
 
       /* Run kernel. */
       kernel_ludcmp (n,
@@ -183,24 +189,29 @@ int main(int argc, char** argv)
          POLYBENCH_ARRAY(x),
          POLYBENCH_ARRAY(y));
 
-      /* Stop and print timer. */
-      polybench_stop_instruments;
-      polybench_print_instruments;
+      #pragma omp master
+      {
+        /* Stop and print timer. */
+        polybench_stop_instruments;
+        polybench_print_instruments;
 
-      /* Prevent dead-code elimination. All live-out data must be printed
-         by the function call in argument. */
-      polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(x)));
-
+        /* Prevent dead-code elimination. All live-out data must be printed
+           by the function call in argument. */
+        polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(x)));
+      }
     }
 
-    /* Be clean. */
-    POLYBENCH_FREE_ARRAY(A);
-    POLYBENCH_FREE_ARRAY(b);
-    POLYBENCH_FREE_ARRAY(x);
-    POLYBENCH_FREE_ARRAY(y);
-
-    printf("###\n");
+    #pragma omp master
+    {
+      printf("###\n");
+    }
   }
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(A);
+  POLYBENCH_FREE_ARRAY(b);
+  POLYBENCH_FREE_ARRAY(x);
+  POLYBENCH_FREE_ARRAY(y);
 
   return 0;
 }
--- shared/lu.h
+++ shared/lu.h
@@ -38,62 +38,59 @@ void swap(double **a, double **b) {
 
 void lu(int n, double *A) {
 #pragma scop
+  double q[BLOCK_SIZE];
 
-  #pragma omp parallel
-  {
-    double q[BLOCK_SIZE];
+  for (int l = 0; l < n - BLOCK_SIZE + 1; l += BLOCK_SIZE) {
+    if (l > 0) {
+      pgemm(n - l, n - l, BLOCK_SIZE,
+            -1, A + l * n + (l - BLOCK_SIZE), n,
+            A + (l - BLOCK_SIZE) * n + l, n,
+            1, A + l * n + l, n);
+    }
 
-    for (int l = 0; l < n - BLOCK_SIZE + 1; l += BLOCK_SIZE) {
-      if (l > 0) {
-        pgemm(n - l, n - l, BLOCK_SIZE,
-              -1, A + l * n + (l - BLOCK_SIZE), n,
-              A + (l - BLOCK_SIZE) * n + l, n,
-              1, A + l * n + l, n);
+    #pragma omp master
+    for (int k = l; k < l + BLOCK_SIZE; k++) {
+      double A_kk = A[k * n + k];
+      for (int i = k + 1; i < l + BLOCK_SIZE; i++) A[i * n + k] /= A_kk;
+
+      for (int i = k + 1; i < l + BLOCK_SIZE; i++) {
+        for (int j = k + 1; j < l + BLOCK_SIZE; j++) {
+          A[i * n + j] -= A[i * n + k] * A[k * n + j];
+        }
       }
+    }
 
-      #pragma omp master
+    #pragma omp barrier
+
+    #pragma omp for
+    for (int u = l + BLOCK_SIZE; u < n - BLOCK_SIZE + 1; u += BLOCK_SIZE) {
       for (int k = l; k < l + BLOCK_SIZE; k++) {
-        double A_kk = A[k * n + k];
-        for (int i = k + 1; i < l + BLOCK_SIZE; i++) A[i * n + k] /= A_kk;
-
         for (int i = k + 1; i < l + BLOCK_SIZE; i++) {
-          for (int j = k + 1; j < l + BLOCK_SIZE; j++) {
+          for (int j = u; j < u + BLOCK_SIZE; j++) {
             A[i * n + j] -= A[i * n + k] * A[k * n + j];
           }
         }
       }
-
-      #pragma omp barrier
-
-      #pragma omp for
-      for (int u = l + BLOCK_SIZE; u < n - BLOCK_SIZE + 1; u += BLOCK_SIZE) {
-        for (int k = l; k < l + BLOCK_SIZE; k++) {
-          for (int i = k + 1; i < l + BLOCK_SIZE; i++) {
-            for (int j = u; j < u + BLOCK_SIZE; j++) {
-              A[i * n + j] -= A[i * n + k] * A[k * n + j];
-            }
-          }
-        }
-      }
-
-      for (int k = l; k < l + BLOCK_SIZE; k++)
-        q[k - l] = 1 / A[k * n + k];
-
-      #pragma omp for
-      for (int i = l + BLOCK_SIZE; i < n; i++) {
-        for (int k = l; k < l + BLOCK_SIZE; k++) {
-          A[i * n + k] *= q[k - l];
-
-          double Aik = A[i * n + k];
-          for (int j = k + 1; j < l + BLOCK_SIZE; j++) {
-            A[i * n + j] -= Aik * A[k * n + j];
-          }
-        }
-      }
-      #pragma omp barrier   
     }
+
+    for (int k = l; k < l + BLOCK_SIZE; k++)
+      q[k - l] = 1 / A[k * n + k];
+
+    #pragma omp for
+    for (int i = l + BLOCK_SIZE; i < n; i++) {
+      for (int k = l; k < l + BLOCK_SIZE; k++) {
+        A[i * n + k] *= q[k - l];
+
+        double Aik = A[i * n + k];
+        for (int j = k + 1; j < l + BLOCK_SIZE; j++) {
+          A[i * n + j] -= Aik * A[k * n + j];
+        }
+      }
+    }
+    #pragma omp barrier   
   }
 
+  #pragma omp master
   if (n % BLOCK_SIZE != 0) {
     int l = n - (BLOCK_SIZE - n % BLOCK_SIZE);
     for (int k = l; k < n; k++) {
